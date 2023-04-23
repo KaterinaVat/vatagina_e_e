7b508d363fcf3712bcbf296443d02bc95ae4cfe3
KatushkaVatagina <113946233+KaterinaVat@users.noreply.github.com>
2023-04-24 00:56:42 +0300

    Update and rename arrayd_test.cpp to arrayd_test_doc.cpp

---
+++ vatagina_e_e/prj.test/arrayd_test_doc.cpp
@@ -0,0 +1,23 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include <arrayd/arrayd.hpp>
+
+TEST_CASE("[arrayd] - ArrayD size and capasity") {
+    ArrayD ab(6);
+    CHECK(ab.ssize() == 6);
+
+    ab.resize(5);
+    CHECK(ab.ssize() == 5);
+
+    ArrayD a(1);
+    CHECK(a[0] == 0);
+
+    a.insert(1,1);
+    CHECK(a[1] == 1);
+
+    a.insert(2, 2);
+    CHECK(a[2] == 2);
+
+    a.remove(2);
+    CHECK(a.ssize() == 2);   
+}
