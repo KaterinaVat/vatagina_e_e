40e656c1c6dab66d16743e49483bdd0b809a4f99
KatushkaVatagina <113946233+KaterinaVat@users.noreply.github.com>
2023-04-30 01:36:43 +0300

    Add files via upload

---
+++ vatagina_e_e/prj.test/arrayt_test.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include "doctest.h"
+#include "arrayt/arrayt.hpp"
+
+
+typedef double T;
+TEST_CASE("[arrayt] - ArrayT size and capasity") {
+    ArrayT<T> ab(6);
+    CHECK(ab.ssize() == 6);
+
+    ab.resize(5);
+    CHECK(ab.ssize() == 5);
+
+    ArrayT<T> a(1);
+    CHECK(a[0] == 0);
+
+    a.insert(1, 1);
+    CHECK(a[1] == 1);
+
+    a.insert(2, 2);
+    CHECK(a[2] == 2);
+
+    a.remove(2);
+    CHECK(a.ssize() == 2);
+}
