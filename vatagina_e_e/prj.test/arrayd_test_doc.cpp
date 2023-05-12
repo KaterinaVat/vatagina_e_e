46e5f7a74006d6f8b6699b738c44388fa6824cc8
KatushkaVatagina <113946233+KaterinaVat@users.noreply.github.com>
2023-05-12 17:16:34 +0300

    Update arrayd_test_doc.cpp

--- vatagina_e_e/prj.test/arrayd_test_doc.cpp
+++ vatagina_e_e/prj.test/arrayd_test_doc.cpp
@@ -2,21 +2,30 @@
 #include <arrayd/arrayd.hpp>
 
 TEST_CASE("[arrayd] - ArrayD size and capasity") {
-    ArrayD ab(6);
+  ArrayD ab(6);
     CHECK(ab.ssize() == 6);
 
     ab.resize(5);
     CHECK(ab.ssize() == 5);
 
-    ArrayD a(1);
+    ArrayD a(5);
     CHECK(a[0] == 0);
 
-    a.insert(1,1);
+    a.insert(1, 1);
     CHECK(a[1] == 1);
 
     a.insert(2, 2);
     CHECK(a[2] == 2);
 
-    a.remove(2);
-    CHECK(a.ssize() == 2);   
+    ArrayD test_array(5);
+    CHECK(test_array.ssize() == 5);
+
+    std::ptrdiff_t last_size = test_array.ssize();
+    test_array[0] = 1;
+    test_array[1] = 2;
+    test_array[2] = 3;
+    test_array[3] = 4;
+    test_array[4] = 5;
+    test_array.remove(2);
+    CHECK(test_array.ssize() == (last_size-1)); 
 }
